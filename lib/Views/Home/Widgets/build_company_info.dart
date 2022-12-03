2f5fe4722db5e30e49d026dd9505a9956183c2c3
unknown <aminkhademi8@gmail.com>
2022-12-03 14:59:16 +0330

    company info mobile responsive completed

--- lib/Views/Home/Widgets/build_company_info.dart
+++ lib/Views/Home/Widgets/build_company_info.dart
@@ -3,6 +3,7 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:pishro_site/Consts/colors.dart';
 import 'package:pishro_site/Consts/measures.dart';
+import 'package:pishro_site/Utils/responsive.dart';
 
 class BuildCompanyInfo extends StatelessWidget {
   const BuildCompanyInfo({Key? key}) : super(key: key);
@@ -23,12 +24,22 @@ class BuildCompanyInfo extends StatelessWidget {
           ),
         ],
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          _buildText(),
-          _buildImage(),
-        ],
+      child: Responsive(
+        mobile: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildTextMobile(),
+            _buildImage(),
+          ],
+        ),
+        desktop: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildText(),
+            _buildImage(),
+          ],
+        ),
+        tablet: SizedBox(),
       ),
     );
   }
@@ -77,6 +88,37 @@ class BuildCompanyInfo extends StatelessWidget {
                     style: TextStyle(
                       color: textPColor,
                       fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextMobile() {
+    return Flexible(
+      flex: 1,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: Get.width * .07),
+        height: double.maxFinite,
+        width: double.maxFinite,
+        child: const AnimationConfiguration.synchronized(
+          duration: Duration(milliseconds: 1500),
+          child: ScaleAnimation(
+            curve: Curves.linearToEaseOut,
+            child: FadeInAnimation(
+              child: Center(
+                child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: Text(
+                    'در سال ۱۳۹۷، جمع شدیم تا از تجربه‌های‌مان در حوزه فرهنگ و فناوری اطلاعات برای ایجاد، توسعه و سرمایه‌گذاری در کسب‌وکارهای هوشمند در حوزه هنر و فرهنگ و ارائه خدمات در این زمینه استفاده کنیم.',
+                    style: TextStyle(
+                      color: textPColor,
+                      fontSize: 16.0,
                     ),
                   ),
                 ),
